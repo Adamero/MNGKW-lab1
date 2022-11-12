46753975e450b459d8388fcfd204806d60327caf
Adamt <adamtrynkiewicz@gmail.com>
2022-11-12 21:26:21 +0100

    modify main v5 + testy

--- test_main.py
+++ test_main.py
@@ -1,10 +1,57 @@
 import unittest
 from unittest import TestCase
-from extractor import Extractor
 import main
-import filecmp
+import csv
 
 
+class Test(TestCase):
 
-if __name__ == "__main__":
+    def odczyt(nrKolumnyDoTestu):
+        kolumnaDoTestu = []
+
+        with open("details.csv", "r", encoding="utf-8") as readCSV:
+            read = csv.reader(readCSV)
+            for line in read:
+                kolumnaDoTestu.append(line[nrKolumnyDoTestu])
+
+        return kolumnaDoTestu
+
+    def test_read_csvfile(self):
+        read_csv_file = main.readCSVFile(0)
+        kolumnaDoTestu = Test.odczyt(0)
+
+        self.assertEqual(read_csv_file, kolumnaDoTestu)
+
+    def test_kolumna1(self):
+        self.assertEqual(main.kolumna1(), Test.odczyt(0))
+
+    def test_kolumna2(self):
+        self.assertEqual(main.kolumna2(), Test.odczyt(1))
+
+    def test_kolumna3(self):
+        self.assertEqual(main.kolumna3(), Test.odczyt(2))
+
+    def test_kolumna4(self):
+        self.assertEqual(main.kolumna4(), Test.odczyt(3))
+
+    def test_kolumna5(self):
+        self.assertEqual(main.kolumna5(), Test.odczyt(4))
+
+    def test_kolumna6(self):
+        self.assertEqual(main.kolumna6(), Test.odczyt(5))
+
+    def test_kolumna7(self):
+        self.assertEqual(main.kolumna7(), Test.odczyt(7))
+
+    def test_kolumna8(self):
+        self.assertEqual(main.kolumna8(), Test.odczyt(8))
+
+    def test_kolumna9(self):
+        self.assertEqual(main.kolumna9(), Test.odczyt(9))
+
+if __name__ == '__main__':
     unittest.main()
+
+
+
+
